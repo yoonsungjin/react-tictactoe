ff428b5193d590e88602c1cdb9debbe9762eeff4
YOON <M.YOON@bycn.bouygues-construction.com>
2019-06-12 09:30:46 -0400

    add history

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ import './index.css';
 
     handleClick(i){
       const squares = this.state.squares.slice();
+      if(calculateWinner(squares) || squares[i]){
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
@@ -39,8 +42,14 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -87,3 +96,22 @@ import './index.css';
     document.getElementById('root')
   );
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
